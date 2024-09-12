1c3de3bfa9f5eedbce27385fe69011f408cd315a
KimHS <best847283@icloud.com>
2024-09-12 17:24:11 +0900

    Level 1 - 3 메서드 분리

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -25,11 +25,8 @@ public class UserService {
 
     @Transactional
     public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
+
+        newPasswordValidation(userChangePasswordRequest);
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
@@ -43,5 +40,12 @@ public class UserService {
         }
 
         user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
+    }
+    private void newPasswordValidation(UserChangePasswordRequest userChangePasswordRequest) {
+        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
+                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
+                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
+            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
+        }
     }
 }
